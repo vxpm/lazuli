6509a9c0cd65480d4ec6c21bf4042984e0409459
Magi <paoloimbrmagi@gmail.com>
2026-03-11 23:49:17 +0100

    fix: cso header file, renaming, code style

--- crates/disks/src/cso.rs
+++ crates/disks/src/cso.rs
@@ -14,19 +14,9 @@ const CSO_MAP_SIZE: usize = CSO_HEADER_SIZE - size_of::<u32>() - 4; // 0x8000 (3
 pub struct CsoHeader {
     #[br(little)]
     /// Size of the blocks
-    block_size: u32,
+    pub block_size: u32,
     /// Used (1) or Unused (0)
-    map: [u8; CSO_MAP_SIZE]
-}
-
-impl CsoHeader {
-    pub fn block_size(&self) -> u32 {
-        self.block_size
-    }
-
-    pub fn map(&self) -> &[u8] {
-        &self.map
-    }
+    pub map: [u8; CSO_MAP_SIZE]
 }
 
 /// A Gamecube .cso file.
@@ -34,7 +24,7 @@ pub struct Cso<R> {
     /// Header of the file
     header: CsoHeader,
     /// LUT
-    cso_map: Vec<Option<u64>>,
+    map: Vec<Option<u64>>,
     /// Reader of the contents
     reader: R
 }
@@ -47,22 +37,22 @@ where
     pub fn new(mut reader: R) -> Result<Self, binrw::Error> {
         let header = CsoHeader::read(&mut reader)?;
 
-        let mut cso_map = Vec::with_capacity(CSO_MAP_SIZE);
+        let mut map= Vec::with_capacity(CSO_MAP_SIZE);
         let mut current_offset = CSO_HEADER_SIZE as u64;
 
-        for &is_present in header.map() {
+        for is_present in header.map {
             if is_present == 1
             {
-                cso_map.push(Some(current_offset));
-                current_offset += header.block_size() as u64;
+                map.push(Some(current_offset));
+                current_offset += header.block_size as u64;
             }
             else
             {
-                cso_map.push(None);
+                map.push(None);
             }
         }
 
-        Ok(Self { header, cso_map, reader })
+        Ok(Self { header, map, reader })
     }
 
     pub fn header(&self) -> &CsoHeader {
@@ -70,7 +60,7 @@ where
     }
 
     pub fn map(&self) -> &Vec<Option<u64>> {
-        &self.cso_map
+        &self.map
     }
 
     pub fn reader(&mut self) -> &mut R {
@@ -80,9 +70,9 @@ where
     /// Read from disk at the given offset and writes it into the output buffer.
     /// Returns how many bytes were actually read.
     pub fn read(&mut self, disk_offset: u64, out: &mut [u8]) -> std::io::Result<u64> {
+        let block_size = self.header().block_size as u64;
         let mut current_disk_offset = disk_offset;
         let mut remaining = out.len() as u64;
-        let block_size = self.header().block_size() as u64;
 
         while remaining > 0 {
 
@@ -93,7 +83,7 @@ where
             let out_start = (current_disk_offset - disk_offset) as usize;
             let out = &mut out[out_start as usize..][..to_read as usize];
 
-            match self.cso_map[block] {
+            match self.map[block] {
                 Some(cso_block) => {
                     self.reader.seek(SeekFrom::Start(cso_block + data_offset))?;
                     self.reader.read_exact(out)?;
@@ -165,7 +155,7 @@ where
                 self.position = self
                     .cso
                     .header()
-                    .block_size()
+                    .block_size
                     .saturating_sub_signed(x as i32) as u64;
             },
             SeekFrom::Current(x) => self.position = self.position.saturating_add_signed(x),
